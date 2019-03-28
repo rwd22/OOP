5f404cc79620331c0301bf1462208b7374b9f984
Ryan W. Douglas <rwd22@zips.uakron.edu>
2019-03-27 20:17:14 -0400

    Simple cout of title and author

--- practice6/main.cpp
+++ practice6/main.cpp
@@ -2,6 +2,14 @@
 #include "json.hpp"
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+std::string get_right_of_delim(std::string const& str, std::string const& delim){
+  return str.substr(str.find(delim) + delim.size());
+}
+
 
 int
 main()
@@ -16,8 +24,54 @@ main()
   std::cout << size(*v) << '\n';
   std::cout << height(*v) << '\n';*/
   
+  std::string filename;
+  
+  bool check = false;
+  
+  std::cout << "Please input json filename" << std::endl;
+  std::cin >> filename;
+  
+  std::ifstream jfile(filename);
+  std::string   line;
+  if(jfile)
+  {
+
+
+		std::string         title;
+		std::string author;
+		std::string test;
+		
+		std::getline (jfile, test);
+		
+		while(test.find("\"author_fullname\": \"") != std::string::npos)
+		{
+			test = get_right_of_delim(test, "\"author_fullname\": \"");
+		
+		author = test.substr(0, test.find("\","));
+		
+		test = get_right_of_delim(test, "\"title\": \"");
+		
+		title = test.substr(0, test.find("\","));
+		
+		//test.substr(0, test.find("\"title\": "));
+		
+		//data = test.substr(0, test.find("\","));
+
+
+		// Read the integers using the operator >>
+		std::cout << title << std::endl;
+		
+		std::cout << author << std::endl;
+		}
+		
+		
+
+  }
+  else
+  {
+	  std::cout << "File Not Found" << std::endl;
+  }
   
   
-  
-  
+  return 0;
 }
